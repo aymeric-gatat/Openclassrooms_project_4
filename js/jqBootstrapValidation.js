ab312fd835c852888cf070fae1b1c30c4a29e4f2
Aymeric <gatataymeric@hotmail.fr>
2023-01-10 10:44:48 +0100

    mis en cache

--- js/jqBootstrapValidation.js
+++ js/jqBootstrapValidation.js
@@ -836,7 +836,7 @@
 			required: {
 				name: "Required",
 				type: "required",
-				message: "This is required<!-- data-validator-required-message to override -->"
+				message: "Ce champ est obligatoire à remplir<!-- data-validator-required-message to override -->"
 			},
 			checkone: {
 				name: "Checkone",
